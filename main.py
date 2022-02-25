894c7ad0dc8f248fd822d9bc59260dc8a98e5c22
patataofcourse <patataofcourse@gmail.com>
2022-02-25 19:35:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def tobtks(tmbin, outfile, tempo=[]):
+    index = int.from_bytes(tmbin.read(4), "little")
+    print(f"Index of file: {hex(index)}")
+
+def unpack(c00, outdir):
+    pass
+
+if __name__ == "__main__":
+    tobtks(open("test_files/in.bin"), open("test_files/out.btk", "wb"))
