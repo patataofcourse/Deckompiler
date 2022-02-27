41e6e665e8c27a5b27aa9a2c0b41e9805cad6af9
patataofcourse <patataofcourse@gmail.com>
2022-02-27 13:13:09 +0100

    file structure shit

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 def tobtks(tmbin, outfile, tempo=[]):
+    #not needed- but nice to print for info purposes
     index = int.from_bytes(tmbin.read(4), "little")
     print(f"Index of file: {hex(index)}")
+
     start = int.from_bytes(tmbin.read(4), "little")
     tmbin.read(4) #Ignore assets sub
     tickflow = bytearray(b"")
 
     # .bin tickflow loop or whatever
-    # copied from tickompiler
+    # copied from tickompiler, modified so that
     pointers = []
     while True:
         cmd = tmbin.read(4)
-        if cmd == b"\xFE\xFF\xFF\xFF":
+        if cmd == b"\xFE\xFF\xFF\xFF": # 0xFFFFFFFE (-2) indicates start of string data
             break
-        str_args = []
-        ptr_args = []
-        if cmd == b"\xFF\xFF\xFF\xFF":
+        str_args = [] #strings and tickflow pointers have to be stored separately
+        ptr_args = [] #because they're managed differently in btks
+        if cmd == b"\xFF\xFF\xFF\xFF": # 0xFFFFFFFF (-1) indicates an 'args' section
             amount = int.from_bytes(tmbin.read(4), "little")
             for _ in range(amount):
                 ann = tmbin.read(4)
@@ -38,12 +40,54 @@ def tobtks(tmbin, outfile, tempo=[]):
     strings = tmbin.read()
 
     stringpos = len(tickflow)
+
     # fix string pointers - stringpos, etc
     for ptr in pointers:
         if ptr[1] != 0: continue
         str_ptr = int.from_bytes(tickflow[ptr[0]:ptr[0]+4], "little") - stringpos
         tickflow[ptr[0]:ptr[0]+4] = str_ptr.to_bytes(4, "little")
     tmbin.close()
+
+    # put everything into sections
+    # 1. FLOW
+    section_flow = {
+        magic: b"FLOW",
+        size: 0xC + len(tickflow),
+        start: start,
+        tickflow: tickflow
+    }
+    # 2. PTRO
+    ptrbin = b""
+    for ptr in pointers:
+        ptrbin += ptr[0].to_bytes(4, "little")
+        ptrbin += ptr[1]
+    section_ptro = {
+        magic: b"PTRO",
+        size: 0xC + len(ptrbin),
+        ptr_amt: len(pointers),
+        pointers: ptrbin,
+    }
+    #TODO: 3. TMPO
+    section_tmpo = None # in the future, only make it None if there's no tempos
+    # 4. STRD
+    section_strd = {
+        magic: b"STRD",
+        size: 0x8 + len(strings),
+        strings: strings
+    }
+
+    #finally, the header!
+    header = {
+        magic: b"BTKS",
+        size: 0x10 + section_flow["size"] + section_ptro["size"] + section_strd["size"],
+        version: 0, #this is rev0 of the BTKS spec
+        section_amt: 3 if section_tmpo == None else 4
+    }
+
+    if section_tmpo != None:
+        header.size += section_tmpo["size"]
+
+    #TODO: put everything into the outfile
 
 
 def unpack(c00, outdir):
