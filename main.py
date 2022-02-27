72125d68c87bd2708809ff1541c61689cc3b0303
patataofcourse <patataofcourse@gmail.com>
2022-02-27 11:56:44 +0100

    finish implementing the tickompiler side

--- main.py
+++ main.py
@@ -14,19 +14,34 @@ def tobtks(tmbin, outfile, tempo=[]):
         adj_args = []
         if cmd == b"\xFF\xFF\xFF\xFF":
             amount = int.from_bytes(tmbin.read(4), "little")
-            for _ in range(arg_count):
-                pass #manage arg annotations
+            for _ in range(amount):
+                ann = tmbin.read(4)
+                anncode = ann[0]
+                ann_arg = int.from_bytes(ann[1:], "little")
+                if (anncode in (0, 1, 2)): #TODO: manage tickflow vs string
+                    adj_args.append(ann_arg)
+            cmd
+            cmd = tmbin.read(4)
         tickflow += cmd
         arg_count = (int.from_bytes(cmd, "little") >> 10) & 0xF
         for _ in range(arg_count):
             arg = tmbin.read(4)
-            if arg[0:3] in adj_args:
-                pass # manage pointer stuff
+            if arg[:3] in adj_args:
+                pass #TODO: manage pointer stuff
             tickflow += arg
+    stringpos = tmbin.tell() - 12
+    print(hex(stringpos))
+    strings = tmbin.read()
+    tmbin.close()
 
 
 def unpack(c00, outdir):
     pass
 
 if __name__ == "__main__":
-    tobtks(open("test_files/in.bin", "rb"), open("test_files/out.btk", "wb"))
+    try:
+        tmbin = open("test_files/in.bin", "rb")
+        tobtks(tmbin, open("test_files/out.btk", "wb"))
+    except Exception as e:
+        print(f"Exception in byte {hex(tmbin.tell())}:")
+        raise e
