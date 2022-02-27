dc80b16114eacc0a413a75cc582a28e372f591bc
patataofcourse <patataofcourse@gmail.com>
2022-02-27 12:17:52 +0100

    tickompiler side almost done

--- main.py
+++ main.py
@@ -9,25 +9,31 @@ def tobtks(tmbin, outfile, tempo=[]):
     # copied from tickompiler
     while True:
         cmd = tmbin.read(4)
-        if cmd == b"\xFE\xFF\xFF\xFF" or cmd == None:
+        if cmd == b"\xFE\xFF\xFF\xFF":
             break
-        adj_args = []
+        str_args = []
+        ptr_args = []
         if cmd == b"\xFF\xFF\xFF\xFF":
             amount = int.from_bytes(tmbin.read(4), "little")
             for _ in range(amount):
                 ann = tmbin.read(4)
                 anncode = ann[0]
                 ann_arg = int.from_bytes(ann[1:], "little")
-                if (anncode in (0, 1, 2)): #TODO: manage tickflow vs string
-                    adj_args.append(ann_arg)
+                if anncode in (0, 1): #TODO: manage tickflow vs string
+                    str_args.append(ann_arg)
+                elif anncode == 2:
+                    ptr_args.append(ann_arg)
             cmd
             cmd = tmbin.read(4)
         tickflow += cmd
         arg_count = (int.from_bytes(cmd, "little") >> 10) & 0xF
-        for _ in range(arg_count):
+        for i in range(arg_count):
             arg = tmbin.read(4)
-            if arg[:3] in adj_args:
-                pass #TODO: manage pointer stuff
+            #TODO: manage those
+            if i in str_args:
+                print("string detected")
+            elif i in ptr_args:
+                print("pointer detected")
             tickflow += arg
     stringpos = tmbin.tell() - 12
     print(hex(stringpos))
