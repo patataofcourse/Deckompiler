cd62c6f310ea25f8e049484719748b7568db4630
patataofcourse <patataofcourse@gmail.com>
2022-03-16 17:10:43 +0100

    revamp string ops

--- deckompiler/tickflow.py
+++ deckompiler/tickflow.py
@@ -1,25 +1,40 @@
+class TickflowCommand:
+    def __init__(self, opcode, arg0):
+        self.opcode = opcode
+        self.arg0 = arg0
+    def __str__(self):
+        return f"{hex(opcode)}" + ("" if arg0 == 0 else (f"{arg0}" if arg0 < 0xA else f"{hex(arg0)}"))
+
+class StringOperation:
+    def __init__(self, command, arguments, scene_id = None):
+        self.command = command
+        self.arguments = arguments
+        if scene_id != None: self.scene_id = scene_id
+
 USTRING_OPS = [
-    (0x31, 0, [1]),
-    (0x35, 0, [1]),
-    (0x39, 0, [1]),
-    (0x3E, 0, [1]),
-    (0x5D, 0, [1]),
-    (0x5D, 2, [0]),
-    (0x61, 2, [0]),
+    StringOperation(TickflowCommand(0x31, 0), [1]),
+    StringOperation(TickflowCommand(0x35, 0), [1]),
+    StringOperation(TickflowCommand(0x39, 0), [1]),
+    StringOperation(TickflowCommand(0x3E, 0), [1]),
+    StringOperation(TickflowCommand(0x5D, 0), [1]),
+    StringOperation(TickflowCommand(0x5D, 2), [0]),
+    StringOperation(TickflowCommand(0x61, 2), [0]),
 ]
 
 ASTRING_OPS = [
-    (0x3B, 0, [2]),
-    (0x67, 1, [1]),
-    (0x93, 0, [2, 3]),
-    (0x94, 0, [1, 2, 3]),
-    (0x95, 0, [1]),
-    (0xB0, 4, [1]),
-    (0xB0, 5, [1]),
-    (0xB0, 6, [1]),
-    (0x66, 0, [1]),
-    (0x65, 1, [1]),
-    (0x68, 1, [1]),
-    (0xAF, 2, [2]),
-    (0xB5, 0, [0]),
+    StringOperation(TickflowCommand(0x3B, 0), [2]),
+    StringOperation(TickflowCommand(0x67, 1), [1]),
+    StringOperation(TickflowCommand(0x93, 0), [2, 3]),
+    StringOperation(TickflowCommand(0x94, 0), [1, 2, 3]),
+    StringOperation(TickflowCommand(0x95, 0), [1]),
+    StringOperation(TickflowCommand(0xB0, 4), [1]),
+    StringOperation(TickflowCommand(0xB0, 5), [1]),
+    StringOperation(TickflowCommand(0xB0, 6), [1]),
+    StringOperation(TickflowCommand(0x66, 0), [1]),
+    StringOperation(TickflowCommand(0x65, 1), [1]),
+    StringOperation(TickflowCommand(0x68, 1), [1]),
+    StringOperation(TickflowCommand(0xAF, 2), [2]),
+    StringOperation(TickflowCommand(0xB5, 0), [0]),
+    StringOperation(TickflowCommand(0x105, 0), [], scene_id=0x0),
+    StringOperation(TickflowCommand(0x10B, 0), [], scene_id=0x0),
 ]
